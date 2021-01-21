2eee41c0ae1fff3050404c82b418df8d923f1505
Bhanu Prasad M <mbhanuprsd@gmail.com>
2021-01-21 17:29:50 +0530

    Implemented vehicle and pedestrian simple AI to follow waypoints

---
+++ Assets/Scripts/SnapshotCamera.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class SnapshotCamera : MonoBehaviour
+{
+    Camera snapCam;
+
+    int resWidth = 256;
+    int resHeight = 256;
+
+    public string filePath;
+
+    void Awake()
+    {
+        snapCam = GetComponent<Camera>();
+        if(snapCam.targetTexture == null)
+        {
+            snapCam.targetTexture = new RenderTexture(resWidth, resHeight, 24);
+        }
+        else
+        {
+            resWidth = snapCam.targetTexture.width;
+            resHeight = snapCam.targetTexture.height;
+        }
+
+        filePath = Application.dataPath + "/../Snapshots/";
+
+        if(!System.IO.Directory.Exists(filePath))
+        {
+            System.IO.Directory.CreateDirectory(filePath);
+        }
+    }
+
+    void Update()
+    {
+            Texture2D snapshot = new Texture2D(resWidth, resHeight, TextureFormat.RGB24, false);
+            snapCam.Render();
+            RenderTexture.active = snapCam.targetTexture;
+            snapshot.ReadPixels(new Rect(0, 0, resWidth, resHeight), 0, 0);
+            byte[] bytes = snapshot.EncodeToJPG();
+            string fileName = SnapShotName();
+            System.IO.File.WriteAllBytes(fileName, bytes);
+            Debug.Log("Snapshot captured - " + fileName);
+    }
+
+    private string SnapShotName()
+    {
+        return string.Format("{0}/snap_{1}x{2}_{3}.jpg",
+            filePath,
+            resWidth, resHeight,
+            System.DateTime.Now.ToString("yyy-MM-dd-HH-mm-ss-SSS"));
+    }
+}
---
+++ Assets/Scripts/WayPointCharacterControl.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WayPointCharacterControl : MonoBehaviour {
+ 
+ 
+ public Transform[] waypoint;        // The amount of Waypoint you want
+ public float patrolSpeed = 3f;       // The walking speed between Waypoints
+ public bool  loop = true;       // Do you want to keep repeating the Waypoints
+ public float dampingLook= 6.0f;          // How slowly to turn
+ public float pauseDuration = 0;   // How long to pause at a Waypoint
+  
+ private float curTime;
+ private int currentWaypoint = 0;
+ private UnityStandardAssets.Characters.ThirdPerson.ThirdPersonCharacter character;
+  
+ void  Start (){
+  
+     character = GetComponent<UnityStandardAssets.Characters.ThirdPerson.ThirdPersonCharacter>();
+ }
+  
+ void  Update (){
+  
+     if(currentWaypoint < waypoint.Length){
+         patrol();
+     }else{    
+         if(loop){
+             currentWaypoint=0;
+         } 
+     }
+ }
+  
+ void  patrol (){
+  
+     Vector3 target = waypoint[currentWaypoint].position;
+     target.y = transform.position.y; // $$anonymous$$eep waypoint at character's height
+     Vector3 moveDirection = target - transform.position;
+  
+     if(moveDirection.magnitude < 0.5f){
+         if (curTime == 0)
+             curTime = Time.time; // Pause over the Waypoint
+         if ((Time.time - curTime) >= pauseDuration){
+             currentWaypoint++;
+             curTime = 0;
+         }
+     }else{        
+         var rotation= Quaternion.LookRotation(target - transform.position);
+         transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * dampingLook);
+         character.Move(moveDirection.normalized * patrolSpeed, false, false);
+     }  
+ }
+ }
